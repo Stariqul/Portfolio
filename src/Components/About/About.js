fb6ca070b6a9c12fe2eb171e1089d8ade1e73a4e
Stariqul <stariqulalam@gmail.com>
2022-06-11 19:35:39 +0600

    about section added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,13 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h2>This is about</h2>
+      <h2>
+        Hi. This is Tariqul Alam. I am an enthusiat learner of front-end
+        developer. I love to explore programming world. Though I am new in this
+        sector , I always find pleasure in learning. <br />I have learnt HTML5,
+        CSS3, Bootstrap, Javascript, ES6, React, React Router Dom, Firebase
+        Authentication. I am familiar with MongoDB, ExpressJs etc
+      </h2>
     </div>
   );
 };
